d79383ca1eb6e6d4c43e4ab681c503f2c7338e28
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-08-08 17:19:34 +0500

    default page button build

--- script.js
+++ script.js
@@ -3,19 +3,16 @@
 //document.querySelector('.guess-message').textContent = 'Правильно!';
 document.querySelector('.number-input').value = 10;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.question').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
     const guesNumber = Number(document.querySelector('.number-input').value)
     console.log(typeof guesNumber);
-
     if (!guesNumber) {
         document.querySelector('.guess-message').textContent = 'Введите число!';
     }   else if (guesNumber === secretNumber) {
         document.querySelector('.guess-message').textContent = 'Правильно!';
+        document.querySelector('.question').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
         document.querySelector('.question').style.width = '50rem';
     } else if (guesNumber > secretNumber) {
@@ -36,4 +33,22 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0;
         }
     }
-})
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.question').textContent = '???';
+    document.querySelector('.question').style.width = '25rem';
+    document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number-input').value = '';
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
+});
+
+
+
+
+
+
+
