e98f849a54cd2d760c55ae6d9ddd3fa14bfd1692
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-08-08 16:22:41 +0500

    score funksiyasi tayyor

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict'
 
-document.querySelector('.guess-message').textContent = 'Правильно!';
+//document.querySelector('.guess-message').textContent = 'Правильно!';
 document.querySelector('.number-input').value = 10;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.question').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guesNumber = Number(document.querySelector('.number-input').value)
@@ -9,6 +14,14 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (!guesNumber) {
         document.querySelector('.guess-message').textContent = 'Введите число!';
+    }   else if (guesNumber === secretNumber) {
+        document.querySelector('.guess-message').textContent = 'Правильно!';
+    } else if (guesNumber > secretNumber) {
+        document.querySelector('.guess-message').textContent = 'Слишком много!';
+        score--;
+    }  else if (guesNumber < secretNumber) {
+        document.querySelector('.guess-message').textContent = 'Слишком мало!';
+        score--;
     }
-
+    document.querySelector('.score').textContent = score;
 })
